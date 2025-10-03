7c3b600b37357a1659c86e3886e50f94688d9ab9
avinash0a <dbaavin@gmail.com>
2025-10-03 18:50:56 +0530

    03101850

--- database/migrations/2025_10_01_134621_create_interview_user_table.php
+++ database/migrations/2025_10_01_134621_create_interview_user_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('interview_user', function (Blueprint $table) {
-            $table->ulid('interview_id');
-            $table->ulid('user_id');
+            $table->uuid('interview_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
             // Composite primary key
